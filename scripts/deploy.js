ca2ee9a01740837f6290eecb85f681b9a851168c
kalinbas <kalinbas@gmail.com>
2022-07-13 15:37:47 -0500

    prepare for final deployment

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -15,6 +15,10 @@ const swapRouterAddress = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
 
 async function main() {
 
+  // set manually for each network
+  const gasPrice = 10000000000
+  const gasLimit = 6000000
+
   const signer = new hre.ethers.Wallet(process.env.DEPLOYMENT_PRIVATE_KEY, hre.ethers.provider)
 
   console.log("Deploying on", hre.network.name)
@@ -22,15 +26,19 @@ async function main() {
   const nativeTokenAddress = nativeTokenAddresses[hre.network.name]
 
   const Contract = await hre.ethers.getContractFactory("Compoundor", signer);
-  const contract = await Contract.deploy(nativeTokenAddress, factoryAddress, nonfungiblePositionManagerAddress, swapRouterAddress);
+  
+  const contract = await Contract.deploy(nativeTokenAddress, factoryAddress, nonfungiblePositionManagerAddress, swapRouterAddress, { gasPrice, gasLimit });
   await contract.deployed();
 
-  await contract.transferOwnership(process.env.MULTISIG_ACCOUNT);
+  //await contract.transferOwnership(process.env.MULTISIG_ACCOUNT);
 
   console.log("Deployed at", contract.address)
 }
 
-// npx hardhat verify --network polygon "0xe3159fc9E26B76De780D7fA1282c438548Ba4E09" "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270" "0x1F98431c8aD98523631AE4a59f267346ea31F984" "0xC36442b4a4522E871399CD717aBDD847Ab11FE88" "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+// npx hardhat verify --network mainnet "" "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2" "0x1F98431c8aD98523631AE4a59f267346ea31F984" "0xC36442b4a4522E871399CD717aBDD847Ab11FE88" "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+// npx hardhat verify --network polygon "" "0x0d500b1d8e8ef31e21c99d1db9a6444d3adf1270" "0x1F98431c8aD98523631AE4a59f267346ea31F984" "0xC36442b4a4522E871399CD717aBDD847Ab11FE88" "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+// npx hardhat verify --network optimism "" "0x4200000000000000000000000000000000000006" "0x1F98431c8aD98523631AE4a59f267346ea31F984" "0xC36442b4a4522E871399CD717aBDD847Ab11FE88" "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+// npx hardhat verify --network arbitrum "" "0x82af49447d8a07e3bd95bd0d56f35241523fbab1" "0x1F98431c8aD98523631AE4a59f267346ea31F984" "0xC36442b4a4522E871399CD717aBDD847Ab11FE88" "0xE592427A0AEce92De3Edee1F18E0157C05861564"
 
 main()
   .then(() => process.exit(0))
